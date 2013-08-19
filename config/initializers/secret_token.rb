87a9d33c9039190886457fdde031c3250911b796
Sumit Gupta <sumitngupta@gmail.com>
2013-08-18 19:31:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CastleLights::Application.config.secret_key_base = '821f824a2a693991af4eaa0e1ae7e5a1081ef345d634bf375a3198bd0898229b501b9190795bb7c9694499a90e26c08b75945386a32239224da594307692c4fd'
